6727b8089755a790311446f6e8aac8d4a5fb8963
Ruk <16yuim@gmail.com>
2023-09-17 14:01:56 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 
 mkdir -p guac/postgres
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > guac/initdb.sql
-wget https://raw.githubusercontent.com/5ym/guacamole-traefik-swarm/main/guac-compose.yml
-vim guac-compose.yml
-docker stack deploy --with-registry-auth -c guac-compose.yml guac
+wget https://raw.githubusercontent.com/5ym/guacamole-traefik-swarm/main/compose.guac.yml
+vim compose.guac.yml
+docker stack deploy --with-registry-auth -c compose.guac.yml guac
